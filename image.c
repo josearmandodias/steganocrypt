4dce23148ba0aa1761f3b22de75117fbf743d440
Jose-Armando Windows <jadlop@protonmail.com>
2026-05-01 22:05:36 +0200

    image part done

--- image.c
+++ image.c
@@ -59,7 +59,7 @@ sImagePPM* charger_ppm(const char* chemin)
     {
         for (int j = 0; j < img->largeur; j++) 
         {
-            fscanf(&img->pixels[i][j], sizeof(sPixel), 1, fp);
+            fread(&img->pixels[i][j], sizeof(sPixel), 1, fp);
         }
     }
 
@@ -76,8 +76,7 @@ int sauvegarder_ppm(const sImagePPM* img, const char* chemin)
     }
 
     fprintf(fp, "P6\n");
-    fprintf(fp, "%d\n", img->largeur);
-    fprintf(fp, "%d\n", img->hauteur);
+    fprintf(fp, "%d %d\n", img->largeur, img->hauteur);
     fprintf(fp, "%d\n", img->max);
 
     for (int i = 0; i < img->hauteur; i++) {
@@ -98,3 +97,124 @@ void liberer_ppm(sImagePPM* img)
     free(img->pixels);
     free(img);
 }
+
+sImagePPM* rotation_90(const sImagePPM* img)
+{
+    sImagePPM* copieimg = malloc(sizeof(sImagePPM));
+    if (!copieimg) 
+    {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    copieimg->largeur = img->hauteur;  // dimensions inversées
+    copieimg->hauteur = img->largeur;
+    copieimg->max = img->max;
+
+    copieimg->pixels = malloc(copieimg->hauteur * sizeof(sPixel*));
+    for (int i = 0; i < copieimg->hauteur; i++) 
+    {
+        copieimg->pixels[i] = malloc(copieimg->largeur * sizeof(sPixel));
+    }
+
+    for (int i = 0; i < img->hauteur; i++)
+    {
+        for (int j = 0; j < img->largeur; j++)
+        {
+            copieimg->pixels[j][copieimg->largeur-1-i] = img->pixels[i][j];
+        }
+    }
+
+    return copieimg;
+}
+
+sImagePPM* symetrie_h(const sImagePPM* img)
+{
+    sImagePPM* copieimg = malloc(sizeof(sImagePPM));
+    if (!copieimg) 
+    {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    copieimg->largeur = img->largeur;
+    copieimg->hauteur = img->hauteur;
+    copieimg->max = img->max;
+
+    copieimg->pixels = malloc(copieimg->hauteur * sizeof(sPixel*));
+    for (int i = 0; i < copieimg->hauteur; i++) 
+    {
+        copieimg->pixels[i] = malloc(copieimg->largeur * sizeof(sPixel));
+    }
+
+    for (int i = 0; i < img->hauteur; i++)
+    {
+        for (int j = 0; j < img->largeur; j++)
+        {
+            copieimg->pixels[i][j] = img->pixels[i][copieimg->largeur-1-j];
+        }
+    }
+
+    return copieimg;
+} // axe vertical
+
+
+sImagePPM* symetrie_v(const sImagePPM* img)
+{
+    sImagePPM* copieimg = malloc(sizeof(sImagePPM));
+    if (!copieimg) 
+    {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    copieimg->largeur = img->largeur;
+    copieimg->hauteur = img->hauteur;
+    copieimg->max = img->max;
+
+    copieimg->pixels = malloc(copieimg->hauteur * sizeof(sPixel*));
+    for (int i = 0; i < copieimg->hauteur; i++) 
+    {
+        copieimg->pixels[i] = malloc(copieimg->largeur * sizeof(sPixel));
+    }
+
+    for (int i = 0; i < img->hauteur; i++)
+    {
+        for (int j = 0; j < img->largeur; j++)
+        {
+            copieimg->pixels[i][j] = img->pixels[copieimg->hauteur-1-i][j];
+        }
+    }
+
+    return copieimg;
+} // axe horizontal
+
+sImagePPM* redimensionner(const sImagePPM* img, int nouv_largeur, int nouv_hauteur)
+{
+    sImagePPM* copieimg = malloc(sizeof(sImagePPM));
+    if (!copieimg) 
+    {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    copieimg->largeur = nouv_largeur;
+    copieimg->hauteur = nouv_hauteur;
+    copieimg->max = img->max;
+
+    copieimg->pixels = malloc(copieimg->hauteur * sizeof(sPixel*));
+    for (int i = 0; i < copieimg->hauteur; i++) 
+    {
+        copieimg->pixels[i] = malloc(copieimg->largeur * sizeof(sPixel));
+    }
+
+    for (int i = 0; i < copieimg->hauteur; i++)
+    {
+        for (int j = 0; j < copieimg->largeur; j++)
+        {
+            copieimg->pixels[i][j] = img->pixels[i * img->hauteur / copieimg->hauteur][j * img->largeur / copieimg->largeur];
+        }
+    }
+
+    return copieimg;
+};
