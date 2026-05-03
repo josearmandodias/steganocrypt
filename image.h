97f44a1002d711665bbed6b72dfaaffbfbb4dee7
Jose-Armando Windows <jadlop@protonmail.com>
2026-05-03 14:28:01 +0200

    traitement done

--- image.h
+++ image.h
@@ -11,6 +11,15 @@ typedef struct
     int largeur, hauteur, max; sPixel** pixels; 
 } sImagePPM;
 
+typedef struct {
+    unsigned char valeur;
+} sPixelGris;
+
+typedef struct {
+    int largeur, hauteur, max;
+    sPixelGris** pixels;
+} sImagePGM;
+
 sImagePPM* charger_ppm(const char* chemin);        
 int        sauvegarder_ppm(const sImagePPM*, const char*);   
 void       liberer_ppm(sImagePPM* img);
